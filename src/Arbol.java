fab627e8d3dd3fe76dddbc85bfbd813e7fac9fc0
Carlos Tapia <carlosviril3@gmail.com>
2022-12-09 11:41:38 +0100

    commit de atribuos arbol

---
+++ src/Arbol.java
@@ -0,0 +1,14 @@
+
+public class Arbol {
+
+	private String nombre;
+	private int altura;
+	
+	public Arbol(String nombre, int altura) {
+		super();
+		this.nombre = nombre;
+		this.altura = altura;
+	}
+	
+	
+}
