8ccf45af74f345340e9cd39fdf641b05fc2f430c
Carlos Tapia <carlosviril3@gmail.com>
2022-12-09 11:45:03 +0100

    metodos clase carlos

--- src/Carlos.java
+++ src/Carlos.java
@@ -9,6 +9,23 @@ public class Carlos {
 		this.color = color;
 		this.edad = edad;
 	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
 	
 	
 }
