25e50e5f73515744c50d3abede09644e080ac26a
Carlos Tapia <carlosviril3@gmail.com>
2022-12-09 11:44:43 +0100

    commit atributos carlos

---
+++ src/Carlos.java
@@ -0,0 +1,14 @@
+
+public class Carlos {
+
+	private String color;
+	private int edad;
+	
+	public Carlos(String color, int edad) {
+		super();
+		this.color = color;
+		this.edad = edad;
+	}
+	
+	
+}
